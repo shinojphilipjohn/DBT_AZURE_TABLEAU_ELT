15bcb635e391fceeff9106c302b615d0e9b54341
Shinoj Philip John <shinoj4005@gmail.com>
2026-05-26 23:08:45 +1000

    models updated

--- dbt_olist/models/mart/fact_order_items.sql
+++ dbt_olist/models/mart/fact_order_items.sql
@@ -3,5 +3,5 @@
 select * from {{ ref('order_items') }}
 
 {% if is_incremental() %}
-where transformed_at > (select max(transformed_at) from {{ this }})
+where transformed_date > (select max(transformed_date) from {{ this }})
 {% endif %}
